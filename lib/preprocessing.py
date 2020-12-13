45553de44f11d3d10f9198e1d6d6000fffb4fe4c
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-12-12 22:00:27 -0800

    added chars/digits preprocessors

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 identity = lambda s: s
-csv =  lambda s: [    v.strip()  for v in s.split(",")    if v.strip()]
-cai =  lambda s: [int(v.strip()) for v in s.split(",")    if v.strip()]
-lsv =  lambda s: [    l.strip()  for l in s.split("\n")   if l.strip()]
-llsv = lambda s: [    l.strip()  for l in s.split("\n\n") if l.strip()]
-lsi =  lambda s: [int(l.strip()) for l in s.split("\n")   if l.strip()]
-ssv =  lambda s: [    v.strip()  for v in s.split(" ")    if v.strip()]
-ssi =  lambda s: [int(v.strip()) for v in s.split(" ")    if v.strip()]
-grid = lambda s: [list(l)        for l in s.split("\n")   if l.strip()]
+
+# space/comma/line separated values/integers
+ssv =  lambda s: [    v.strip()  for v in s.strip().split(" ")   ]
+ssi =  lambda s: [int(v.strip()) for v in s.strip().split(" ")   ]
+csv =  lambda s: [    v.strip()  for v in s.strip().split(",")   ]
+csi =  lambda s: [int(v.strip()) for v in s.strip().split(",")   ]
+lsv =  lambda s: [    l.strip()  for l in s.strip().split("\n")  ]
+lsi =  lambda s: [int(l.strip()) for l in s.strip().split("\n")  ]
+llsv = lambda s: [    l.strip()  for l in s.strip().split("\n\n")]
+
+# grids
+grid = lambda s: [list(l) for l in s.strip().split("\n")]
+
+# characters/digit strings
+characters = lambda s: [    c  for c in list(s.strip())]
+digits     = lambda s: [int(d) for d in list(s.strip())] 
