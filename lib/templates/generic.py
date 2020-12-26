6b53abcd366bd68f48e9177243d65bdae4204b52
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-12-25 16:37:10 -0800

    moved templates

---
+++ lib/templates/generic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lib import *
+problem = aoc.Problem("2015/01: ???")
+problem.preprocessor = ppr.lsv
+
+
+@problem.solver()
+def solve(inp):
+    p1, p2 = 0, 0
+
+    for v in inp:
+        print(v)
+
+    return (0, 0)
+
+
+if __name__ == "__main__":
+    problem.solve()
