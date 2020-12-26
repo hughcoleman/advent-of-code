59ea8b76b0701431886ec596059b104f52e66fc7
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-12-25 17:20:32 -0800

    reordered imports

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import lib.ntheory as nt
-import lib.preprocessing as ppr
+
 import lib.problem as aoc
-from lib.geometry import Point
 
 # import auxillary libraries
+import lib.preprocessing as ppr
+import lib.ntheory as nt
+from lib.geometry import Point
