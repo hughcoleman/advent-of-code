b0028a37f091eda5a9d7615dae97bca41760b0d7
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2021-01-07 21:26:43 -0800

    2015/16

---
+++ events/2015/16.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lib import *
+problem = aoc.Problem("2015/16: Aunt Sue")
+problem.preprocessor = lambda aunts: {
+    # extract aunt number
+    int(aunt[aunt.index(" ") + 1 : aunt.index(":")]): {
+        compound.split(": ")[0]: int(compound.split(": ")[1])
+        for compound in aunt.split(": ", 1)[1].split(", ")
+    }
+    for aunt in aunts.strip().split("\n")
+}
+
+READINGS = {
+    "children": 3,
+    "cats": 7,
+    "samoyeds": 2,
+    "pomeranians": 3,
+    "akitas": 0,
+    "vizslas": 0,
+    "goldfish": 5,
+    "trees": 3,
+    "cars": 2,
+    "perfumes": 1,
+}
+
+
+@problem.solver(part=1)
+def naive(aunts):
+    for n, aunt in aunts.items():
+        for compound, amount in aunt.items():
+            if READINGS[compound] != amount:
+                break
+        else:
+            return n
+
+    raise RuntimeError("Unsolvable! No matching aunts found.")
+
+
+COMPARATORS = {
+    # conditions are inverted, because we're "accepting the negative"
+    "cats": lambda e, a: e >= a,
+    "trees": lambda e, a: e >= a,
+    "pomeranians": lambda e, a: e <= a,
+    "goldfish": lambda e, a: e <= a,
+}
+
+
+@problem.solver(part=2)
+def actual(aunts):
+    for n, aunt in aunts.items():
+        for compound, amount in aunt.items():
+            comparator = COMPARATORS.get(compound, lambda e, a: e != a)
+
+            if comparator(READINGS[compound], amount):
+                break
+        else:
+            return n
+
+    raise RuntimeError("Unsolvable! No matching aunts found.")
+
+
+if __name__ == "__main__":
+    problem.solve()
