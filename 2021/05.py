2d55f4edada276599a86e6ddc40e7074617265fc
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2021-12-12 13:18:07 -0800

    2021/05

---
+++ 2021/05.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" 2021/05: Hydrothermal Venture """
+
+import sys
+import collections as cl
+
+vents = [
+    tuple(
+        tuple( int(coordinate) for coordinate in point.split(",") )
+            for point in ln.strip().split(" -> ")
+    )
+        for ln in sys.stdin.read().strip().split("\n")
+]
+
+floor = cl.defaultdict(int)
+
+for (x1, y1), (x2, y2) in vents:
+    if (x1 == x2) or (y1 == y2):
+        for d in range(abs(x2 - x1) + abs(y2 - y1) + 1):
+            floor[min(x1, x2) + d*(x1 != x2), min(y1, y2) + d*(y1 != y2)] += 1
+
+print("Part 1:", sum(x > 1 for x in floor.values()))
+
+for (x1, y1), (x2, y2) in vents:
+    if (x1 != x2) and (y1 != y2):
+        dx, dy = (1 if x2 > x1 else -1), (1 if y2 > y1 else -1)
+        for d in range(abs(x2 - x1) + 1):
+            floor[x1 + dx*d, y1 + dy*d] += 1
+
+print("Part 2:", sum(x > 1 for x in floor.values()))
