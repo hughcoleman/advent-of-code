c39a82c11202447d3d429dfbbef6e7fb9df9065e
hughcoleman <htcolema@uwaterloo.ca>
2022-12-03 00:14:52 -0500

    2022/03

---
+++ 2022/03.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 2022/03: Rucksack Reorganization """
+
+import string
+import sys
+
+rucksacks = sys.stdin.read().strip().split("\n")
+
+def priority(x):
+    x, = x
+    return (string.ascii_lowercase + string.ascii_uppercase).index(x) + 1
+
+print("Part 1:", sum(
+    priority(set(rucksack[:len(rucksack) // 2]) & set(rucksack[len(rucksack) // 2:]))
+        for rucksack in rucksacks
+))
+print("Part 2:", sum(
+    priority(set.intersection(*map(set, rucksacks[i:i + 3])))
+        for i in range(0, len(rucksacks), 3)
+))
